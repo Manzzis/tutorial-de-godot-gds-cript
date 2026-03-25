d547ec752e8e4b22d43e506825e26f2fe6c001b4
Manzzis <mati.manzano.exe@gmail.com>
2026-03-24 21:07:51 -0300

    HUD created
    creado hud, pantalla de inicio y game over, y script hud y actualizacion en el script de main para integrar e instanciar las funciones y señales de HUD

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,12 +16,16 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$StartTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
 	$Player.Start($StarPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
@@ -42,8 +46,13 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
+
+
+func _on_player_hit() -> void:
+	game_over()
